922cd442d46b46fc4e4fdb870c6a0e9278ffab94
Ahmed Mahmoud Azzam <gamerazzam@gmail.com>
2021-09-02 14:51:51 +0200

    Finished Anwered Question

---
+++ src/app/AnsweredQuestion.js
@@ -0,0 +1,71 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { selectQuestions } from "../features/questions/questionsSlice";
+import {
+  selectAllUsers,
+  selectCurrentUser,
+} from "../features/users/usersSlice";
+
+const AnsweredQuestion = (props) => {
+  const { id } = props;
+  const currentUser = useSelector(selectCurrentUser);
+  const users = useSelector(selectAllUsers);
+  const questions = useSelector(selectQuestions);
+  const question = questions[id];
+  const asker = users[question.author];
+  //Votes count
+  const voteOneCount = question.optionOne.votes.length;
+  const voteTwoCount = question.optionTwo.votes.length;
+  const totalVotes = voteOneCount + voteTwoCount;
+  //Precentage
+  const precentageOne = ((voteOneCount / totalVotes) * 100).toFixed(1);
+  const precentageTwo = (100 - precentageOne).toFixed(1);
+  //Current user's Answer
+  const currentUserAnswer = currentUser.answers[id];
+  const choseOptionOne = currentUserAnswer === "optionOne";
+
+  return (
+    <div className="question-answered-container">
+      <div className="answered-question-asked-by">{`Asked by ${asker.name}`}</div>
+      <img className="answered-question-user-avatar" src={asker.avatarURL} />
+      <div className="answered-question-header">
+        <h2>Results:</h2>
+      </div>
+      <div
+        className={`answered-result-1 ${
+          choseOptionOne
+            ? "answered-question-winner"
+            : "answered-question-loser"
+        }`}>
+        {choseOptionOne ? (
+          <span className="your-answer">Your Answer</span>
+        ) : null}
+        <div className="answered-result-text">{question.optionOne.text}</div>
+        <div className="meter progress-bar" id="">
+          <div style={{ width: `${precentageOne}%` }}>
+            <span className="loading-bar progress">{`${precentageOne}%`}</span>
+          </div>
+        </div>
+        <div className="answered-result-ratio">{`${voteOneCount} out of ${totalVotes}`}</div>
+      </div>
+      <div
+        className={`answered-result-2 ${
+          !choseOptionOne
+            ? "answered-question-winner"
+            : "answered-question-loser"
+        }`}>
+        {!choseOptionOne ? (
+          <span className="your-answer">Your Answer</span>
+        ) : null}
+        <div className="answered-result-text">{question.optionTwo.text}</div>
+        <div className="meter progress-bar" id="">
+          <div style={{ width: `${precentageTwo}%` }}>
+            <span className="loading-bar progress">{`${precentageTwo}%`}</span>
+          </div>
+        </div>
+        <div className="answered-result-ratio">{`${voteTwoCount} out of ${totalVotes}`}</div>
+      </div>
+    </div>
+  );
+};
+export default AnsweredQuestion;
--- src/app/Question.js
+++ src/app/Question.js
@@ -1,14 +1,14 @@
 import React from "react";
-
 import { useParams } from "react-router-dom";
+import AnsweredQuestion from "./AnsweredQuestion";
 
 import UnansweredQuestion from "./UnansweredQuestion";
 const Question = () => {
   const { id, isAnswered } = useParams();
-  if (isAnswered === "true") {
-    return <div>Under Development</div>;
-  } else if (isAnswered === "false") {
-    return <UnansweredQuestion id={id} />;
-  }
+  return isAnswered === "true" ? (
+    <AnsweredQuestion id={id} />
+  ) : (
+    <UnansweredQuestion id={id} />
+  );
 };
 export default Question;
