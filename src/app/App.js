36813d2cdd72cc0c4bcacb4c3e66e6bd7f221869
Ahmed Mahmoud Azzam <gamerazzam@gmail.com>
2021-09-02 08:56:12 +0200

    structure change

---
+++ src/app/App.js
@@ -0,0 +1,59 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from "react-router-dom";
+import { useSelector } from "react-redux";
+import "../App.css";
+import { Login } from "./login";
+import Home from "./Home";
+import NotFound from "./NotFound";
+import Question from "./Question";
+import NavBar from "./NavBar";
+import { selectCurrentUser } from "../features/users/usersSlice";
+function App() {
+  let currentUser = useSelector(selectCurrentUser);
+  return (
+    <Router>
+      <Route
+        path="/"
+        render={() => {
+          return (
+            <>
+              <NavBar key="navbar" />{" "}
+              <Redirect
+                to={{
+                  pathname: "/login",
+                }}
+              />
+            </>
+          );
+        }}
+      />
+      <Switch>
+        <Route exact path="/login" key="login" component={Login} />
+        <Route exact path="/home" key="home" component={Home} />
+        <Route
+          exact
+          path="/questions/:id"
+          render={() =>
+            currentUser ? (
+              <Question />
+            ) : (
+              <Redirect
+                to={{
+                  pathname: "/login",
+                }}
+              />
+            )
+          }
+        />
+        <Route path="*" key="notfound" component={NotFound} />
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,43 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./app/App";
 import "./index.css";
-import App from "./App";
 import { store } from "./app/store";
 import { Provider } from "react-redux";
-import {
-  BrowserRouter as Router,
-  Redirect,
-  Route,
-  Switch,
-} from "react-router-dom";
 import * as serviceWorker from "./serviceWorker";
-import NavBar from "./app/NavBar";
-import { Login } from "./app/login";
-import Home from "./app/Home";
-import NotFound from "./app/NotFound";
-import Question from "./app/Question";
-
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <Router>
-        <Route
-          path="/"
-          render={() => {
-            return (
-              <>
-                <NavBar key="navbar" /> <Redirect to="login" />
-              </>
-            );
-          }}
-        />
-        <Switch>
-          <Route exact path="/login" key="login" component={Login} />
-          <Route exact path="/home" key="home" component={Home} />
-          <Route exact path="/questions/:id" component={Question} />
-          <Route path="*" key="notfound" component={NotFound} />
-        </Switch>
-      </Router>
+      <App />
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
